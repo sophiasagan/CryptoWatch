5a9b76c7024eb1e125b20f4424a6cfc614bcc038
Sophia Jung <sophiabraddock82@gmail.com>
2021-01-22 19:04:39 -0500

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import CoinDetail from './Pages/CoinDetail'
+import Dashboard from './Pages/Dashboard'
 
-function App() {
+import {BrowserRouter, Route} from 'react-router-dom'
+import Header from './Components/Header';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <BrowserRouter>
+      <Header />
+      <Route exact path='/' component={Dashboard} />
+      </BrowserRouter>
+     
     </div>
   );
 }
---
+++ src/Pages/CoinDetail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CoinDetail() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CoinDetail
